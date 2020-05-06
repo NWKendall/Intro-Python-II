82f519e629205193d3d5e87adad0f7ad2a381fd8
nicbongo <nicbongo@protonmail.com>
2020-05-06 20:19:06 -0300

    nesting happy path overlook & foyer

--- src/adv.py
+++ src/adv.py
@@ -4,7 +4,7 @@ from player import Player
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mouth beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -38,6 +38,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player("", "", "", 100)
 
 # Write a loop that:
 #
@@ -49,18 +50,55 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
-player = Player("", "")
-while True:
-    player.name = input("Welcome hero, what is your name? ")
-    start_game = input(f"If the perils become to much, you can escape by pressing 'Q'. Are you ready to begin {player.name}? (y/n) ")
 
-    if start_game.lower().strip() == "n":
+player.name = input("Welcome hero, what is your name? ")
+while player.hp > 0:
+
+    start_game = input(f"If the perils become to much, you can escape by pressing 'Q'. Are you ready to begin {player.name}? (y/q) ")
+    if start_game.lower().strip() == "q":
         print("See you soon!")
         break
-    else:
-        player.room = room['outside']
-        print(f"You find yourself",player.room)
 
+    player.current_room = room['outside']
+    print(f"You find yourself",player.current_room,"...")
+    directionInput = input("You can move by pressing 'N', 'S', 'E', 'W' to navigate. Enter the cave when you're ready...")
+   
+   
+    try:
+        if directionInput.lower().strip() == "q":
+            print("CHICKEN!")
+            break
+        elif directionInput.lower().strip() != "n":
+            print("So inert on the looming cave, you forgot about the type rope you were walking and fell to a grizzly death")
+            player.hp = 0
+            break
+        # navigate to foyer ✅
+        elif directionInput.lower().strip() == "n":
+            print(f"**{player.name} walked north...**")
+            print(f"You find yourself in the {player.current_room.n_to}...")
+            player.current_room = room['foyer']
+            directionInput = input("Which way?")
 
+            try:
 
+                # navigate to overlook
+                if directionInput.lower().strip() != "n":
+                    print("So inert on the looming cave, you forgot about the type rope you were walking and fell to a grizzly death")
+                    player.hp = 0
+                    directionInput = input("Which way?")
+                    
+        # navigate to narrow ✅
+        elif directionInput.lower().strip() == "e":
+            print(f"**{player.name} walked east...**")
+            print(f"You find yourself in the {player.current_room.e_to}...")
+            player.current_room = player.current_room.e_to
+            directionInput = input("Which way?")
+        # navigate to outside
+        elif directionInput.lower().strip() == "s":
+            print(f"**{player.name} walked south...**")
+            print(f"You find yourself in the {player.current_room.s_to}...")
+            player.current_room = player.current_room.s_to
+            directionInput = input("Which way?")
 
+    except ValueError:
+        print("asdas")
--- src/player.py
+++ src/player.py
@@ -2,12 +2,13 @@
 # currently.
 
 class Player:
-    def __init__(self, name, room, items=["Torch"]):
-        self.name = name
-        self.room = room
-        self.items = items
+    def __init__(self, room_name, current_room, direction, hp):
+        self.room_name = room_name
+        self.current_room = current_room
+        self.direction = direction
+        self.hp = hp
 
     def __str__(self):
-        return f"{self.name} is in {self.room}"
+        return f"{self.name} is in {self.current_room}"
 
 
